dcd00145e3b9d12e1b1d18c69c11dc51374c54a2
Albert Boehmler <ajboehmler@gmail.com>
2013-02-20 17:31:01 -0500

    Converting to use flask web framework

--- test_server.py
+++ test_server.py
@@ -1,88 +1,61 @@
-import web
-from web.contrib.template import render_jinja
-import test_forms
-import compare_data
+
 import verify_lists as vl
-from datetime import datetime
+import compare_data
 
-render = render_jinja(
-        'templates',   # Set template directory.
-        encoding = 'utf-8',                         # Encoding.
-    )
-urls = (
-    '/verify_lists', 'verify_lists',
-    '/compare', 'compare',
-    '/compare_with_tolerance', 'compare_with_tolerance',
-    '/favicon.ico', 'favicon',
-    '/(.*)', 'index'
-)
-app = web.application(urls, globals())
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
 
-def get_log_file_name():
-        log_file_name = "%s_%s_%s_%s_%s_%s.log" % datetime.timetuple(datetime.now())[0:6]
-        return log_file_name
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/verify_lists', methods=['GET', 'POST'])
+def verify_lists():
+    if request.method == 'GET':
+        return render_template('verify_lists.html')
+
+    expected_list = request.form['expected_list']
+    actual_list = request.form['actual_list']
+    compare_type = request.form['compare_type']
+    log_file_name = get_log_file_name()
+    vl.init_logging(log_file_name)
+    results = vl.verify_lists(expected_list, actual_list,
+                              compare_type, True)
+    vl.stop_logging
+    f = open(log_file_name)
+    contents = f.read()
+    f.close()
+
+    return render_template('verify_lists_results.html',
+                           content=contents)
+
+@app.route('/compare', methods=['GET', 'POST'])
+def compare():
+    if request.method == 'GET':
+        return render_template('data_comparison.html')
+
+    prod_data = request.form['prod_data']
+    int_data = request.form['int_data']
+    results = compare_data.compare_tabular_inputs(prod_data, int_data)
     
+    return render_template('data_comparison_results.html',
+                           content=results)
 
-class compare:
-    def GET(self):
-        return render.data_comparison()
+@app.route('/compare_with_tolerance', methods=['GET', 'POST'])
+def compare_with_tolerance():
+    if request.method == 'GET':
+        return render_template('data_comparison_results_with_tolerance.html')
 
-    def POST(self):
-        prod_data = web.input()["prod_data"]
-        int_data = web.input()["int_data"]
-        results = compare_data.compare_tabular_inputs(prod_data, int_data)
-        return render.data_comparison_results(content=results)
-
-
-class compare_with_tolerance:
-    def GET(self):
-        return render.data_comparison_results_with_tolerance()
-
-    def POST(self):
-        prod_data = web.input()["prod_data"]
-        int_data = web.input()["int_data"]
-        results = compare_data.compare_tabular_inputs_with_tolerance(prod_data, int_data)
-        return render.data_comparison_results(content=results)
-
-
-class verify_lists:
-    def GET(self):
-        return render.verify_lists()
-
-    def POST(self):
-        expected_list = web.input()["expected_list"]
-        actual_list = web.input()["actual_list"]
-        compare_type = web.input()["compare_type"]
-
-        log_file_name = get_log_file_name()
-        vl.init_logging(log_file_name)
-        results = vl.verify_lists(expected_list, actual_list,
-                                  compare_type, True)
-        vl.stop_logging
-        f = open(log_file_name)
-        contents = f.read()
-        f.close()
-
-        #return contents
-        return render.verify_lists_results(content=contents)
-
-
-class index:
-    def GET(self, path):
-        return render.index(
-            title="Testing Tools and Services",
-            contents="Hello, world!",
-            )
-
-f = open('static/favicon.ico', 'rb')
-favicon_ico = f.read()
-f.close()
-
-class favicon:
-    def GET(self):
-        web.header("Content-Type", "image/x-icon") 
-        return favicon_ico
+    prod_data = request.form['prod_data']
+    int_data = request.form['int_data']
+    results = compare_data.compare_tabular_inputs_with_tolerance(prod_data,
+                                                                 int_data)
+    return render_template('data_comparison_results.html',
+                           content=results)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',
+            debug=True)
