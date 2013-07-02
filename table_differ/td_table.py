ef3fe337d89f49305b59c6ab96559571be4f4314
Bennett Mead <bmead_a,t,chathamfinancial.com>
2013-07-02 17:35:14 -0400

    Clean up the conversion of unicode to ascii
    The unicodedata package does a great job with a best representation of
    unicode in ascii, so we can use this instead of struggling with specific
    code page issues.

--- table_differ/td_table.py
+++ table_differ/td_table.py
@@ -1,3 +1,4 @@
+import unicodedata
 class TdTable:
 
     def __init__(self):
@@ -9,7 +10,7 @@ class TdTable:
     def get_value(self, row_index, col_index, as_str=True):
         v = self._rows[row_index][col_index]
         if as_str:
-            v = str(v)
+            v = unicodedata.normalize('NFKD', unicode(v)).encode('ascii', 'ignore')
         return v
 
     @property
