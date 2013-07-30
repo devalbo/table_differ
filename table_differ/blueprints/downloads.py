9f4c6b577e2caf519c01217ca03454a918a3c9d9
Albert Boehmler <ajboehmler@gmail.com>
2013-07-30 01:08:39 -0400

    Issue #8 - download actual table as CSV file

--- table_differ/blueprints/downloads.py
+++ table_differ/blueprints/downloads.py
@@ -9,14 +9,14 @@ blueprint = Blueprint('downloads', __name__,
                       template_folder='templates')
 
 @blueprint.route('/comparisons/<int:comparison_id>/tables/actual')
-def downloads_actual_table(comparison_id):
+def download_actual_table(comparison_id):
     actual_table = models.ComparisonResult.get(models.ComparisonResult.id == comparison_id).actual_table_csv
 
     strIO = StringIO.StringIO()
-    strIO.write(actual_table)
+    strIO.write(str(actual_table))
     strIO.seek(0)
     return send_file(strIO,
-                     attachment_filename="actual-table-%s.png" % comparison_id,
+                     attachment_filename="actual-table-%s.csv" % comparison_id,
                      as_attachment=True)
 
 
